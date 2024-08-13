799426adefa701ceb012970a6e7bcad043ed5746
VictorG10 <victorgbadebo10@gmail.com>
2024-08-13 17:27:40 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup></script>
+
+<template>
+  <main>
+    <RouterView />
+  </main>
+</template>
+
+<style lang="scss" scoped>
+  .container {
+    max-width: 1100px;
+    margin: 0 auto;
+  }
+</style>
